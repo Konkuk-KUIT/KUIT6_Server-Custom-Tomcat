822abc51e57c89f7f0c82e60185ef55aacc6e509
ParkSeongGeun <phd0801@naver.com>
2025-09-29 18:01:02 +0900

    [Refact] Refact RequestHandler

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,16 +2,10 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import controller.UserSignupController;
-import controller.UserLoginController;
-import controller.UserListController;
-import controller.StaticFileController;
+import controller.*;
 import http.HttpRequest;
 import http.HttpResponse;
 import http.enums.HttpMethod;
@@ -20,6 +14,7 @@ import http.enums.RequestPath;
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
+    private Controller controller = new ForwardController();
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
@@ -36,44 +31,26 @@ public class RequestHandler implements Runnable{
             HttpRequest httpRequest = HttpRequest.from(br);
             log.log(Level.INFO, "Request Line: " + httpRequest.getMethod() + " " + httpRequest.getPath() + " " + httpRequest.getVersion());
 
-            String method = httpRequest.getMethod().getValue();
-            String path = httpRequest.getPath();
-            String queryString = httpRequest.getQueryString();
-            String cookieValue = httpRequest.getCookie();
-            String requestBody = httpRequest.getBody();
-
-            log.log(Level.INFO, "Method: " + method + ", Path: " + path + ", Query String: " + queryString);
-            if (cookieValue != null) {
-                log.log(Level.INFO, "Cookie received: " + cookieValue);
-            }
-            if (requestBody != null) {
-                log.log(Level.INFO, "Request body: " + requestBody);
+            // Controller 선택 로직
+            if (httpRequest.getPath().equals(RequestPath.ROOT.getValue())) {
+                controller = new ForwardController();
             }
 
-            // 경로에 따른 파일 매핑 로직
-            // 회원 가입 처리
-            if (path.equals(RequestPath.USER_SIGNUP.getValue()) && HttpMethod.POST.getValue().equals(method)) {
-                UserSignupController controller = new UserSignupController();
-                controller.execute(httpRequest, httpResponse);
-                return;
+            if (httpRequest.getPath().equals(RequestPath.USER_SIGNUP.getValue()) && 
+                httpRequest.getMethod() == HttpMethod.POST) {
+                controller = new UserSignupController();
             }
 
-            // 로그인 처리 (POST 방식만)
-            if (path.equals(RequestPath.USER_LOGIN.getValue()) && HttpMethod.POST.getValue().equals(method)) {
-                UserLoginController controller = new UserLoginController();
-                controller.execute(httpRequest, httpResponse);
-                return;
+            if (httpRequest.getPath().equals(RequestPath.USER_LOGIN.getValue()) && 
+                httpRequest.getMethod() == HttpMethod.POST) {
+                controller = new UserLoginController();
             }
 
-            // userList 경로 처리
-            if (path.equals(RequestPath.USER_LIST.getValue())) {
-                UserListController controller = new UserListController();
-                controller.execute(httpRequest, httpResponse);
-                return;
+            if (httpRequest.getPath().equals(RequestPath.USER_LIST.getValue())) {
+                controller = new UserListController();
             }
 
-            // 정적 파일 처리 (모든 나머지 요청)
-            StaticFileController controller = new StaticFileController();
+            // Controller 실행
             controller.execute(httpRequest, httpResponse);
 
         } catch (IOException e) {
