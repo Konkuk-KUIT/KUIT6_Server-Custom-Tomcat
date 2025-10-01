ef23ed142abb59d58b96f5e883ec4a0a448c75bc
andyn <andycho1120@naver.com>
2025-10-01 15:53:33 +0900

    Tomcat구현 1단계 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,7 @@ package webserver;
 
 import db.MemoryUserRepository;
 import http.util.HttpRequestUtils;
+import http.util.IOUtils;
 import model.User;
 
 import java.io.*;
@@ -20,8 +21,8 @@ public class RequestHandler implements Runnable{
     public RequestHandler(Socket connection) {
         this.connection = connection;
     }
-
-
+    boolean isCorrectPassword;
+    boolean isLoggedIn = false;
     @Override
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
@@ -59,34 +60,129 @@ public class RequestHandler implements Runnable{
             String method = stringStringMap.get("method");
             String path = stringStringMap.get("path");
             String queryString = stringStringMap.get("queryString");
-            Map<String, String> paramMap = HttpRequestUtils.parseQueryParams(queryString);
-            System.out.println("method = " + method);
-            System.out.println("path = " + path);
-            System.out.println("paramMap = " + paramMap);
-
-// sign-up 요청: http://localhost/user/signup?userId=userid&password=password&name=name&email=email%40k.com
-            if(Objects.equals(path, "/user/signup") && Objects.equals(method,"POST")){
-                User user = new User(paramMap.get("userId"),
-                        paramMap.get("password"),
-                        paramMap.get("name"),
-                        paramMap.get("email"));
-                MemoryUserRepository.getInstance().addUser(user);
+//            System.out.println("method = " + method);
+//            System.out.println("path = " + path);
+//            Map<String, String> paramMap = HttpRequestUtils.parseQueryParams(queryString);
+//            System.out.println("method = " + method);
+//            System.out.println("path = " + path);
+//            System.out.println("paramMap = " + paramMap);
+            if(Objects.equals(path, "/")){
                 path="index.html";
             }
-            if(Objects.equals(path, "")){
-                path="index.html";
+
+            else if(Objects.equals(path, "/user/signup") && Objects.equals(method,"POST")){
+                int contentLength = 0;
+                while (true) {
+                    final String line = br.readLine();
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    // header info
+                    if (line.startsWith("Content-Length")) {
+                        contentLength = Integer.parseInt(line.split(": ")[1]);
+                    }
+                }
+
+                char[] bodyData = new char[contentLength];
+                br.read(bodyData, 0, contentLength);
+
+                String body = new String(bodyData);
+                Map<String, String> bodyParams = HttpRequestUtils.parseQueryParams(body);
+
+                User user = new User(bodyParams.get("userId"),
+                        bodyParams.get("password"),
+                        bodyParams.get("name"),
+                        bodyParams.get("email"));
+                MemoryUserRepository.getInstance().addUser(user);
+            }
+            else if(Objects.equals(path, "/user/login") && Objects.equals(method,"POST")){
+                int contentLength = 0;
+                while (true) {
+                    final String line = br.readLine();
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    // header info
+                    if (line.startsWith("Content-Length")) {
+                        contentLength = Integer.parseInt(line.split(": ")[1]);
+                    }
+                }
+
+                char[] bodyData = new char[contentLength];
+                br.read(bodyData, 0, contentLength);
+
+                String body = new String(bodyData);
+                Map<String, String> bodyParams = HttpRequestUtils.parseQueryParams(body);
+                String userId = bodyParams.get("userId");
+                String password = bodyParams.get("password");
+//                System.out.println("userId = " + userId);
+//                System.out.println("password = " + password);
+                User userById = MemoryUserRepository.getInstance().findUserById(userId);
+                isCorrectPassword = userById != null && userById.getPassword().equals(password);
+//                System.out.println("isCorrectPassword = " + isCorrectPassword);
+            }
+            else if(Objects.equals(path, "/user/userList") && Objects.equals(method,"GET")){
+                String requestCookie;
+                while (true) {
+                    final String line = br.readLine();
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    // header info
+                    if (line.startsWith("Cookie")) {
+                        requestCookie = line.split(": ")[1];
+//                        System.out.println("requestCookie = " + requestCookie);
+                        String cookieValue = IOUtils.getCookieValue(requestCookie, "logined");
+                        isLoggedIn = cookieValue != null && cookieValue.equals("true");
+                    }
+                }
+
             }
 
             byte[] body;
 
-            if(Objects.equals(path, "/user/signup")){
+            if(Objects.equals(path, "/user/signup")&& Objects.equals(method,"POST")){
                 body=new byte[]{};
                 response302Header(dos, "/index.html");
+                responseEndHeader(dos);
+            }
+            else if(Objects.equals(path, "/user/login") && Objects.equals(method,"POST")){
+                if(isCorrectPassword){
+                    body=new byte[]{};
+                    response302Header(dos, "/index.html");
+                    responseLoginCookieHeader(dos);
+                    responseEndHeader(dos);
+                }
+                else{
+                    body=new byte[]{};
+                    response302Header(dos,"/user/login_failed.html");
+                    responseEndHeader(dos);
+                }
+            }
+            else if(Objects.equals(path, "/user/userList") && Objects.equals(method,"GET")){
+                if(isLoggedIn){
+                    body= Files.readAllBytes(Paths.get("./webapp/"+"user/list.html"));
+                    response200Header(dos, body.length);
+                    responseEndHeader(dos);
+                }
+                else{
+                    body=new byte[]{};
+                    response302Header(dos, "/index.html");
+                    responseEndHeader(dos);
+                }
+            }
+            else if (Objects.equals(path, "/css/style.cs") && Objects.equals(method,"GET")){
+                body=Files.readAllBytes(Paths.get("./webapp/css/style.css"));
+                responseCssHeader(dos, body.length);
+                responseEndHeader(dos);
             }
             else{
-                 body= Files.readAllBytes(Paths.get("./webapp/"+ path));
+                body= Files.readAllBytes(Paths.get("./webapp/"+ path));
                 response200Header(dos, body.length);
+                responseEndHeader(dos);
             }
+//            System.out.println("dos = " + dos);
+//            System.out.println("body = " + body);
             responseBody(dos, body);
 
         } catch (IOException e) {
@@ -94,25 +190,44 @@ public class RequestHandler implements Runnable{
         }
     }
 
+    private void responseCssHeader(DataOutputStream dos, int lengthOfBodyContent) throws IOException {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \rz\n");
+            dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \rz\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
     }
 
+    private void responseLoginCookieHeader(DataOutputStream dos) {
+        try{
+            dos.writeBytes("Set-Cookie: logined=true\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+    private void responseEndHeader(DataOutputStream dos) {
+        try {
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
     private void response302Header(DataOutputStream dos, String path) {
         try {
             dos.writeBytes("HTTP/1.1 302 Found \rz\n");
-//            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Location: " + path + "\r\n");
-            dos.writeBytes("Content-Length: 0\r\n");
 
-            dos.writeBytes("\r\n");
 
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
