48f1e6b0c90fb5eaba66997a28c01b05c9d16fd9
hwanggyuun <gka365@naver.com>
2025-10-02 14:52:21 +0900

    feat: 기능 2 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,14 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import http.util.HttpRequestUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,11 +27,36 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
+
             String requestLine = br.readLine();
-            String url = "/index.html";  // 예시
-            byte[] body = Files.readAllBytes(Paths.get("./webapp" + url));
-            response200Header(dos, body);
-            responseBody(dos, body);
+            System.out.println(requestLine);
+            String[] tokens = requestLine.split(" ");
+            String method = tokens[0]; // GET
+            String path = tokens[1];
+            if (path.equals("/") || path.equals("/index.html")) {
+                // index.html 반환
+                byte[] body = Files.readAllBytes(Paths.get("./webapp/index.html"));
+                response200Header(dos, body);
+                responseBody(dos, body);
+            }
+            else if (path.equals("/user/form.html")) {
+                // 회원가입 폼 반환
+                byte[] body = Files.readAllBytes(Paths.get("./webapp/user/form.html"));
+                response200Header(dos, body);
+                responseBody(dos, body);
+            }
+            else if (path.startsWith("/user/signup")) {
+                Map<String, String> params = HttpRequestUtils.parseQueryParameter(path);
+                String userId = params.get("userId");
+                String password = params.get("password");
+                String name = params.get("name");
+                String email = params.get("email");
+                User user = new User(userId, password, name, email);
+                MemoryUserRepository userDB = MemoryUserRepository.getInstance();
+                userDB.addUser(user);
+                response302Header(dos, "/index.html");
+            }
+
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
@@ -46,6 +76,8 @@ public class RequestHandler implements Runnable{
         }
     }
 
+
+
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
@@ -55,4 +87,14 @@ public class RequestHandler implements Runnable{
         }
     }
 
+    private void response302Header(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
 }
