314626e61ce7b0a316c50cbea51552d6f4a6bab1
yeonzeop <ksp2617@naver.com>
2025-10-02 22:08:33 +0900

    feat: 요구사항 1-5,6 (더티 코딩...)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -58,12 +58,39 @@ public class RequestHandler implements Runnable {
             }
 
             if (path.contains("/user/signup")) {
-//                String[] pathTokens = path.split("\\?");
-//                Map<String, String> userInfos = HttpRequestUtils.parseQueryParameter(pathTokens[1]);
-//                MemoryUserRepository db = MemoryUserRepository.getInstance();
-//                db.addUser(new User(userInfos.get("userId"), userInfos.get("password"), userInfos.get("name"), userInfos.get("email")));
-//                response302Header(dos, "/index.html");
 
+                MemoryUserRepository db = MemoryUserRepository.getInstance();
+                int requestContentLength = 0;
+
+                while (true) {
+                    final String line = br.readLine();
+                    if (line.equals("")) {
+                        break;
+                    }
+                    // header info
+                    if (line.startsWith("Content-Length")) {
+                        requestContentLength = Integer.parseInt(line.split(": ")[1]);
+                    }
+                }
+                Map<String, String> userInfos = HttpRequestUtils
+                        .parseQueryParameter(IOUtils.readData(br, requestContentLength));
+                db.addUser(new User(userInfos.get("userId")
+                        , userInfos.get("password"), userInfos.get("name"), userInfos.get("email")));
+                log.info("가입완료" + userInfos.get("userId"));
+                log.info("userId: " + db.findUserById(userInfos.get("userId")).getUserId());
+                response302Header(dos, "/index.html");
+
+            }
+
+            if (path.equals("/user/login.html")) {
+                File file = new File("webapp/user/login.html");
+                int fileLength = (int) file.length();
+                response200Header(dos, fileLength);
+                responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
+            }
+
+            if (path.equals("/user/login")) {
+                int requestContentLength = 0;
                 MemoryUserRepository db = MemoryUserRepository.getInstance();
                 while (true) {
                     final String line = br.readLine();
@@ -71,16 +98,80 @@ public class RequestHandler implements Runnable {
                         break;
                     }
                     if (line.startsWith("Content-Length")) {
-                        int requestContentLength = Integer.parseInt(line.split(": ")[1]);
-                        Map<String, String> userInfos = HttpRequestUtils
-                                .parseQueryParameter(IOUtils.readData(br, requestContentLength));
-                        db.addUser(new User(userInfos.get("userId")
-                                , userInfos.get("password"), userInfos.get("name"), userInfos.get("email")));
-                        response302Header(dos, "/index.html");
+                        requestContentLength = Integer.parseInt(line.split(": ")[1]);
                     }
                 }
+                Map<String, String> userInfos = HttpRequestUtils
+                        .parseQueryParameter(IOUtils.readData(br, requestContentLength));
+                User user = db.findUserById(userInfos.get("userId"));
+                if (user == null) {
+                    response302Header(dos, "/user/login_failed.html");
+                    return;
+                }
+                if (user.getPassword().equals(userInfos.get("password"))) {
+                    responseLoginHeader(dos, "/index.html");
+                } else response302Header(dos, "/user/login_failed.html");
             }
 
+            if (path.equals("/user/login_failed.html")) {
+                File file = new File("webapp/user/login_failed.html");
+                int fileLength = (int) file.length();
+                responseLoginFailedHeader(dos, fileLength);
+                responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
+            }
+
+            if(path.equals("/user/userList")){
+                String logined = "";
+                while (true) {
+                    final String line = br.readLine();
+                    if (line.equals("")) {
+                        break;
+                    }
+                    if (line.startsWith("Cookie: ")) {
+                        String cookieHeader = line.substring("Cookie:".length()).trim();
+                        String[] pairs = cookieHeader.split(";");
+                        for (String pair : pairs) {
+                            String[] kv = pair.trim().split("=", 2); // 최대 2개만 split
+                            if (kv.length == 2) {
+                                String name = kv[0].trim();
+                                String value = kv[1].trim();
+                                if (name.equals("logined")) {
+                                    logined = value;
+                                }
+                            }
+                        }
+                    }
+                }
+                if(logined.equals("true")){
+                    File file = new File("webapp/user/list.html");
+                    int fileLength = (int) file.length();
+                    response200Header(dos, fileLength);
+                    responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
+                }
+            }
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void responseLoginFailedHeader(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Set-Cookie: logined=; Path=/; Max-Age=0; HttpOnly; SameSite=Lax\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void responseLoginHeader(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("Set-Cookie: logined=true; Path=/; HttpOnly; SameSite=Lax\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
