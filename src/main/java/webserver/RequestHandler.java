b1d49acb88fb08155a32d13d81e76a7d64851eb4
yeonzeop <ksp2617@naver.com>
2025-10-02 12:20:28 +0900

    feat: 요구사항 1-1 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,10 +2,12 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class RequestHandler implements Runnable{
+public class RequestHandler implements Runnable {
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
 
@@ -16,16 +18,41 @@ public class RequestHandler implements Runnable{
     @Override
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String requestLine = br.readLine();
+            log.info("Request Line: " + requestLine);
+            if (requestLine == null || requestLine.isEmpty()) return;
+
+            String[] tokens = requestLine.split(" ");
+            String method = tokens[0];
+            String path = tokens[1];
+
+            if (path.equals("/") || path.equals("/index.html")) {
+                File file = new File("webapp/index.html");
+                int fileLength = (int) file.length();
+                response200Header(dos, fileLength);
+                responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
+            }
+
+            if(path.equals("/qna/show.html")){
+                File file = new File("webapp/qna/show.html");
+                int fileLength = (int) file.length();
+                response200Header(dos, fileLength);
+                responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
+            }
+
+            if(path.equals("/user/form.html")){
+                File file = new File("webapp/user/form.html");
+                int fileLength = (int) file.length();
+                response200Header(dos, fileLength);
+                responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
+            }
 
         } catch (IOException e) {
-            log.log(Level.SEVERE,e.getMessage());
+            log.log(Level.SEVERE, e.getMessage());
         }
     }
 
