62fe1dd3f7ed3954063e4e7ae1bc1b10b27545c7
소희 <y04828@naver.com>
2025-10-03 01:11:38 +0900

    [feat] 2단계 요구사항 3 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,17 +2,10 @@ package webserver;
 
 import db.MemoryUserRepository;
 import enums.*;
-import http.HttpBody;
-import http.HttpHeaderMap;
-import http.HttpRequest;
-import http.HttpStartLine;
+import http.*;
 import model.User;
-
 import java.io.*;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,7 +27,7 @@ public class RequestHandler implements Runnable {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            DataOutputStream dos = new DataOutputStream(out);
+            HttpResponse response = new HttpResponse(out);
 
             // HttpRequest 파싱
             HttpRequest httpRequest = HttpRequest.from(br);
@@ -55,7 +48,7 @@ public class RequestHandler implements Runnable {
                 Map<String, String> params = startLine.getQueryParams();
                 saveUser(params);
                 // index.html로 리다이렉트
-                response302Header(dos, RequestPath.INDEX.getPath());
+                response.sendRedirect(RequestPath.INDEX.getPath());
                 return;
             }
 
@@ -63,7 +56,7 @@ public class RequestHandler implements Runnable {
             if (method == POST && RequestPath.SIGNUP.matches(path)) {
                 Map<String, String> params = body.getFormData();
                 saveUser(params);
-                response302Header(dos, RequestPath.INDEX.getPath());
+                response.sendRedirect(RequestPath.INDEX.getPath());
                 return;
             }
 
@@ -78,39 +71,33 @@ public class RequestHandler implements Runnable {
                 User user = repository.findUserById(userId);
 
                 if (user != null && user.getPassword().equals(password)) { // 로그인 성공했으면
-                    response302LoginSuccess(dos, RequestPath.INDEX.getPath());
+                    response.sendRedirectWithCookie(RequestPath.INDEX.getPath(), "logined=true; Path=/");
                 } else
-                    response302Header(dos, RequestPath.USER_LOGIN_FAILED.getPath());
+                    response.sendRedirect(RequestPath.USER_LOGIN_FAILED.getPath());
                 return;
             }
 
             // 사용자 목록 출력
             if (RequestPath.USER_LIST.matches(path)) {
                 if (header.isLogined()) { // 로그인 되어있다면 userList.html 반환
-                    String filePath = "webapp" + RequestPath.USER_LIST.getPath();
-                    byte[] responseBody = Files.readAllBytes(Paths.get(filePath));
-                    response200Header(dos, responseBody.length, filePath);
-                    responseBody(dos, responseBody);
+                    response.forward(RequestPath.USER_LIST.getPath());
                 } else {
-                    response302Header(dos, RequestPath.LOGIN.getPath()+".html");
+                    response.sendRedirect(RequestPath.LOGIN.getPath() + ".html");
                 }
                 return;
             }
 
-            String filePath = "webapp" + path; // URL 경로 → 프로젝트 폴더 경로로 매핑
+            //정적 파일 응답
             try {
-                byte[] responseBody = Files.readAllBytes(Paths.get(filePath));
-                response200Header(dos, responseBody.length, filePath); // 응답 헤더: 200 OK
-                responseBody(dos, responseBody);             // 응답 바디: 파일 내용
+               response.forward(path);
             } catch (IOException e) {
-                response404Header(dos);
+                response.sendNotFound();
             }
 
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
     }
-
 
     private static void saveUser(Map<String, String> params) {
         User user = new User(
@@ -123,60 +110,4 @@ public class RequestHandler implements Runnable {
         // 파싱한 데이터로 User 객체 만들어준다.
         MemoryUserRepository.getInstance().addUser(user);
     }
-
-    private void response404Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes(HttpStatus.NOT_FOUND.toStatusLine());
-            dos.writeBytes(HttpHeader.CONTENT_TYPE.getValue() + ": text/html;charset=utf-8\r\n");
-            dos.writeBytes("\r\n");
-            dos.writeBytes("<h1>404 Not Found</h1>");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
-    private void response302LoginSuccess(DataOutputStream dos, String path) {
-        try {
-            dos.writeBytes(HttpStatus.FOUND.toStatusLine());
-            dos.writeBytes(HttpHeader.LOCATION.getValue() + ": " + path + "\r\n");
-            dos.writeBytes(HttpHeader.SET_COOKIE.getValue() + ": logined=true\r\n"); // 로그인 여부 쿠키 저장
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String path) {
-        try {
-            dos.writeBytes(HttpStatus.FOUND.toStatusLine());            dos.writeBytes(HttpHeader.LOCATION.getValue() + ": " + path + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String path) {
-        try {
-            dos.writeBytes(HttpStatus.OK.toStatusLine());
-            if (path.endsWith(".css")) { // .css 요청이 오면 브라우저가 CSS로 인식해서 스타일 적용
-                dos.writeBytes(HttpHeader.CONTENT_TYPE.getValue() + ": text/css\r\n");
-            } else {
-                dos.writeBytes(HttpHeader.CONTENT_TYPE.getValue() + ": text/html;charset=utf-8\r\n");
-            }
-            dos.writeBytes(HttpHeader.CONTENT_LENGTH.getValue() + ": " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
-        }
-    }
-
 }
