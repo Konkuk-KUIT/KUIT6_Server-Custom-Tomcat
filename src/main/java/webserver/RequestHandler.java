4f9def33e541714f4092eeb6dbc2d78b7f0bee89
yeonzeop <ksp2617@naver.com>
2025-10-02 12:51:11 +0900

    feat: 요구사항 1-2, 회원가입

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,9 +1,14 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import http.util.HttpRequestUtils;
+import model.User;
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,23 +42,35 @@ public class RequestHandler implements Runnable {
                 responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
             }
 
-            if(path.equals("/qna/show.html")){
+            if (path.equals("/qna/show.html")) {
                 File file = new File("webapp/qna/show.html");
                 int fileLength = (int) file.length();
                 response200Header(dos, fileLength);
                 responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
             }
 
-            if(path.equals("/user/form.html")){
+            if (path.equals("/user/form.html")) {
                 File file = new File("webapp/user/form.html");
                 int fileLength = (int) file.length();
                 response200Header(dos, fileLength);
                 responseBody(dos, Files.readAllBytes(Paths.get(file.getPath())));
             }
 
+            if (path.contains("/users/signup")) {
+                String[] pathTokens = path.split("\\?");
+                Map<String, String> userInfos = HttpRequestUtils.parseQueryParameter(pathTokens[1]);
+                MemoryUserRepository db = MemoryUserRepository.getInstance();
+                db.addUser(new User(userInfos.get("userId"), userInfos.get("password"), userInfos.get("name"), userInfos.get("email")));
+                response302Header(dos, "/index.html");
+            }
+
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
+    }
+
+    private void response302Header(DataOutputStream dos, String path) {
+
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
