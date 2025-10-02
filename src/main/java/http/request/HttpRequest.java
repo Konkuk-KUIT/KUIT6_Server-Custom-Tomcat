458841891620b0852cd45eb449b13977f8b4820e
hyungyu-02 <yona20020209@gmail.com>
2025-10-03 01:14:04 +0900

    feat: (2단계 요구사항 2) create HttpRequest

---
+++ src/main/java/http/request/HttpRequest.java
@@ -0,0 +1,59 @@
+package http.request;
+
+import http.enums.HttpHeader;
+import http.enums.HttpMethod;
+import http.util.HttpRequestUtils;
+import http.util.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private final HttpMethod method;
+    private final String path;
+    private final Map<String, String> headers;
+    private final Map<String, String> params;
+
+    private HttpRequest(HttpMethod method, String path, Map<String, String> headers, Map<String, String> params) {
+        this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.params = params;
+    }
+
+    public static HttpRequest from(BufferedReader br) throws IOException {
+        String requestLine = br.readLine();
+        if (requestLine == null || requestLine.isEmpty()) {
+            throw new IOException("Empty request line");
+        }
+        String[] requestLineTokens = requestLine.split(" ");
+        HttpMethod method = HttpMethod.valueOf(requestLineTokens[0]);
+        String path = requestLineTokens[1];
+
+        Map<String, String> headers = new HashMap<>();
+        String line;
+        while ((line = br.readLine()) != null && !line.isEmpty()) {
+            String[] headerTokens = line.split(": ");
+            if (headerTokens.length == 2) {
+                headers.put(headerTokens[0], headerTokens[1]);
+            }
+        }
+
+        Map<String, String> params = new HashMap<>();
+        String contentLengthValue = headers.get(HttpHeader.CONTENT_LENGTH.getValue());
+        if (contentLengthValue != null) {
+            int contentLength = Integer.parseInt(contentLengthValue);
+            String bodyString = IOUtils.readData(br, contentLength);
+            params = HttpRequestUtils.parseQueryParameter(bodyString);
+        }
+
+        return new HttpRequest(method, path, headers, params);
+    }
+
+    public HttpMethod getMethod() { return method; }
+    public String getPath() { return path; }
+    public Map<String, String> getHeaders() { return headers; }
+    public Map<String, String> getParams() { return params; }
+}
