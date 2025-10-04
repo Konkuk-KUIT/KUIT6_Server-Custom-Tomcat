ebabb3c6b0f29416002dbcfa4e30c585acc43ea4
hwanggyuun <gka365@naver.com>
2025-10-04 12:40:47 +0900

    fix: 요구사항 5 구현완

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,9 @@ import constant.Url;
 import db.MemoryUserRepository;
 import http.util.HttpRequestUtils;
 import model.User;
+import webserver.controller.Controller;
+import webserver.controller.ControllerFactory;
+import webserver.controller.StaticFileController;
 
 import java.io.*;
 import java.net.Socket;
@@ -30,89 +33,24 @@ public class RequestHandler implements Runnable{
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()){
             HttpRequest request = new HttpRequest(in);
+            HttpResponse response = new HttpResponse();
             DataOutputStream dos = new DataOutputStream(out);
-            MemoryUserRepository userDB = MemoryUserRepository.getInstance();
+
             String path = request.getPath();
-            String method = request.getMethod();
-            int contentLength = request.getContentLength();
-            boolean isLogin = request.isLogin();
-            String body = request.getBody();
+            Controller controller = ControllerFactory.getController(path);
 
-
-
-
-            //여기가 지금
-            if (path.equals(Url.ROOT.path()) || path.equals(Url.INDEX.path())) {
-                // index.html 반환
-
-                byte[] bodyFile = Files.readAllBytes(Paths.get(Url.ROOT.filePath()));
-                HttpResponse.writeResponse(dos, HttpStatus.OK,HttpContentType.HTML.value(), bodyFile);
-            }
-            else if (path.endsWith(".css")) {
-                byte[] bodyFile = Files.readAllBytes(Paths.get(Url.CSS.filePath() + path));
-                HttpResponse.writeResponse(dos, HttpStatus.OK, HttpContentType.CSS.value(), bodyFile);
-
-            }
-            else if (path.equals(Url.USER_FORM.path())) {
-                // 회원가입 폼 반환
-                byte[] bodyFile = Files.readAllBytes(Paths.get(Url.USER_FORM.filePath()));
-                HttpResponse.writeResponse(dos, HttpStatus.OK,HttpContentType.HTML.value(), bodyFile);
-
-            }
-            else if (path.startsWith(Url.USER_SIGNUP.path())) {
-
-                Map<String, String> params = HttpRequestUtils.parseQueryParameter(body);
-                String userId = params.get("userId");
-                String password = params.get("password");
-                String name = params.get("name");
-                String email = params.get("email");
-                userDB.addUser(new User(userId, password, name, email));
-                User existingUser = userDB.findUserById(userId);
-                HttpResponse.writeRedirect(dos,Url.INDEX.path());
-            }
-            else if (path.equals(Url.USER_LOGIN_HTML.path())) {
-                byte[] bodyFile = Files.readAllBytes(Paths.get(Url.USER_LOGIN_HTML.filePath()));
-                HttpResponse.writeResponse(dos, HttpStatus.OK, HttpContentType.HTML.value(), bodyFile);
-
-            }
-            else if(path.equals(Url.USER_LOGIN_FAILED.path())){
-                byte[] bodyFile = Files.readAllBytes(Paths.get(Url.USER_LOGIN_FAILED.filePath()));
-                HttpResponse.writeResponse(dos, HttpStatus.OK,HttpContentType.HTML.value(), bodyFile);
-
+            if (controller == null) {
+                // css, js 등 정적 파일 처리용
+                controller = new StaticFileController();
             }
 
-            else if (path.equals(Url.USER_LOGIN.path())) {
-                if (method.equals("POST")) {
-                    Map<String, String> params = HttpRequestUtils.parseQueryParameter(body);
+            controller.service(dos, request, response);
 
-                    String userId = params.get("userId");
-                    String password = params.get("password");
-
-                    User existingUser = userDB.findUserById(userId);
-                    // 로그인 검증
-                    if (existingUser != null) {
-                        // 로그인 성공
-                        HttpResponse.writeRedirectWithCookie(dos,Url.INDEX.path(), "logined=true");
-                    } else {
-                        // 로그인 실패
-                        HttpResponse.writeRedirect(dos,Url.USER_LOGIN_FAILED.path());
-                    }
-                }
-            }else if (path.equals(Url.USER_LIST.path())) {
-                // 자 여긴 만약에 했을때임
-                if(isLogin){
-                    byte[] bodyFile = Files.readAllBytes(Paths.get(Url.USER_LIST.filePath()));
-                    HttpResponse.writeResponse(dos, HttpStatus.OK,HttpContentType.HTML.value(), bodyFile);
-                    System.out.println("아니 성공은 했딴다");
-                }else{
-                    HttpResponse.writeRedirect(dos,Url.USER_LOGIN_FAILED.path());
-                    System.out.println("넌 실패했어");
-                }
-
-            }
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
--- src/main/java/webserver/controller/StaticFileController.java
+++ src/main/java/webserver/controller/StaticFileController.java
@@ -7,12 +7,13 @@ import webserver.HttpRequest;
 import webserver.HttpResponse;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class StaticFileController implements Controller {
     @Override
-    public void service(DataOutputStream dos, HttpRequest request, HttpResponse response) {
+    public void service(DataOutputStream dos, HttpRequest request, HttpResponse response) throws IOException {
         String path = request.getPath();
         byte[] bodyFile = Files.readAllBytes(Paths.get(Url.CSS.filePath() + path));
         HttpResponse.writeResponse(dos, HttpStatus.OK, HttpContentType.CSS.value(), bodyFile);
